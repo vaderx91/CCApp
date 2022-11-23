6e55ad60f13b80275449fa459fce82f922d5c694
Anton <A@email.com>
2022-11-23 23:49:17 +0300

    1

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.gluonhq.charm.glisten;
     requires com.fasterxml.jackson.databind;
     requires org.json;
     requires tess4j;
